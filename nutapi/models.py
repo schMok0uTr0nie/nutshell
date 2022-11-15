df807df84ddad58cea762323cf146daa0a5036d8
Tonio <toha.futbolist@gmail.com>
2022-11-15 22:09:23 +1000

    (15/11/22) JScript

--- nutapi/models.py
+++ nutapi/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Nut(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
+    added_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
--- nutshell/urls.py
+++ nutshell/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('nutapi.urls')),
+    path('', include('nutfront.urls')),
 ]
